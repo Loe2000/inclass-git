52c30bbf98d953e610c83c28b5c2372331465827
Erik Lo <loe@oregonstate.edu>
2021-04-15 16:50:13 -0700

    subtraction

--- calc.py
+++ calc.py
@@ -1,4 +1,12 @@
 def calc(a, b):
     sum = a + b
-    print(sum)
+    difference = a - b
+
+
+#Write the code for subtract
+#Do a commit
+#Write the code for multiply
+#Do a commit
+#Write code for division
+#Do a commit
 
