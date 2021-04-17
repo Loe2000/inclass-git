a0bd555cae8bf0f1e6b90c02c44a15819418f8e4
Erik Lo <loe@oregonstate.edu>
2021-04-16 22:32:38 -0700

    Now allows user input for length of the password

--- password.py
+++ password.py
@@ -1,11 +1,11 @@
-#Password generator - Write a program that takes in a number as input and returns a password of that length.
 import random
 import string
 
-def password(a):
+def password():
+    a = int(input('Input the length of your password (integer): '))
     for x in range(a):
         string.ascii_letters
         print(random.choice(string.ascii_letters), end="")
 
 
-password(7)
+password()
