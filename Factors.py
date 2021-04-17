1251ffde059c2092bf99cdfa627ac855f5708cac
Erik Lo <loe@oregonstate.edu>
2021-04-16 22:02:42 -0700

    added user input for factors

--- Factors.py
+++ Factors.py
@@ -1,9 +1,9 @@
-def factors(a):
+def factors():
+    a = int(input('Input an integer: '))
     print('The factors of ', a, ' are:')
     for x in range(a//2):
         if a % (x + 1) == 0:
             print(x + 1)
     print(a)
 
-factors(7)
-factors(12)
+factors()
